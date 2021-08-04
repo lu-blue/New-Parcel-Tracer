98c725858d965bc6a4d740d7fa0b3bedbb2b657a
Lubov <lubov,novozhilova@gmail.com>
2021-08-04 11:24:54 +0200

    navbar and footer color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,15 @@ import SmallLogo from "../assets/parcel-logoo-nob.png";
 export default function Footer() {
   return (
     <footer className="footer">
-      
-<br/>
+      {/* <br/> */}
       <div className="footer__link">
         <h2>
           <a href="mailto:lubov.novozhilova@gmail.com">Contact us</a>
         </h2>
-      
-      <Link to="/">
-      <img className="logo" src={SmallLogo} alt="Small blue parcel" />
-      </Link>
+
+        <Link to="/">
+          <img className="logo" src={SmallLogo} alt="Small blue parcel" />
+        </Link>
       </div>
     </footer>
   );
